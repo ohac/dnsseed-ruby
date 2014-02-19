9cae14a8dcec84b50c4019253eadbb30a8f74553
OHASHI Hideya <ohachige@gmail.com>
2014-02-19 20:52:21 +0900

    refactoring

--- bitcoin-scan.rb
+++ bitcoin-scan.rb
@@ -4,36 +4,49 @@ require 'bitcoin-node'
 #require 'config'
 #require 'global'
 
-host = '127.0.0.1'
-
-port = 9301
-timeout = 30
-min_last_seen = 1 * 24 * 60 * 60
-
-origNode = BitcoinNode.new(host, port, timeout)
-
-nodes = origNode.getAddr
-begin
-  if !nodes.empty?
-    #start_db_transaction
-    #add_node_to_dns(host, port, origNode.getVersion)
-    t = Time.now.to_i - min_last_seen
-    nodes.each do |node|
-      if node[:services][0] == 3 && node[:services][1] == 0
-        if node[:timestamp] > t
-p [Time.at(node[:timestamp]), node[:ipv4], node[:port]]
-          #add_untested_node(node[:ipv4], node[:port])
-        end
+def scan(host = '127.0.0.1', port = 9301, timeout = 30, min_last_seen = 24)
+  origNode = BitcoinNode.new(host, port, timeout)
+  nodes = origNode.getAddr
+  return if nodes.empty?
+  t = Time.now.to_i - min_last_seen * 60 * 60
+  nodes.each do |node|
+    if node[:services][0] == 3 && node[:services][1] == 0
+      if node[:timestamp] > t
+        yield node
       end
     end
-    #commit_db_transaction
-  else
-    #start_db_transaction
-    #remove_node(host, port)
-    #commit_db_transaction
   end
-rescue
-  #start_db_transaction
-  #remove_node(host, port)
-  #commit_db_transaction
+  origNode
 end
+
+def start_db_transaction
+end
+
+def commit_db_transaction
+end
+
+def add_untested_node(ipv4, port)
+p [:add_untested_node, ipv4, port]
+end
+
+def add_node_to_dns(host, port, version)
+p [:add_node_to_dns, host, port, version]
+end
+
+def remove_node(host, port)
+p [:remove_node, host, port]
+end
+
+start_db_transaction
+begin
+  host = '127.0.0.1'
+  port = 9301
+  node = scan(host, port) do |node|
+p [Time.at(node[:timestamp]), node[:ipv4], node[:port]]
+    add_untested_node(node[:ipv4], node[:port])
+  end
+  add_node_to_dns(host, port, node.getVersion) if node
+rescue
+  remove_node(host, port)
+end
+commit_db_transaction
