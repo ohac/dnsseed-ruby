d5fe31b79024cedb2376af2566417b5622658b46
OHASHI Hideya <ohachige@gmail.com>
2014-02-19 20:39:07 +0900

    coding

--- bitcoin-scan.rb
+++ bitcoin-scan.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/ruby
-$LOAD_PATH.unshift File.dirname(__FILE__) # + '/../lib'
+$LOAD_PATH.unshift File.dirname(__FILE__)
 require 'bitcoin-node'
 #require 'config'
 #require 'global'
@@ -17,15 +17,13 @@ begin
   if !nodes.empty?
     #start_db_transaction
     #add_node_to_dns(host, port, origNode.getVersion)
+    t = Time.now.to_i - min_last_seen
     nodes.each do |node|
       if node[:services][0] == 3 && node[:services][1] == 0
-        if node[:timestamp] >= Time.now.to_i - min_last_seen
-          puts 'claiming old node...'
+        if node[:timestamp] > t
+p [Time.at(node[:timestamp]), node[:ipv4], node[:port]]
+          #add_untested_node(node[:ipv4], node[:port])
         end
-p [Time.at(node[:timestamp]), node[:ipv4], node[:port], node[:services]]
-        #add_untested_node(node[:ipv4], node[:port])
-      else
-        puts "Not accepted."
       end
     end
     #commit_db_transaction
